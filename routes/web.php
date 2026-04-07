cf0b99904c66ae6ac1c8af5d6e136b077cab84b6
Karen Lin <linmei7918@gmail.com>
2026-04-06 18:29:55 -0600

    fixed profile photo delete fail issue

--- routes/web.php
+++ routes/web.php
@@ -170,5 +170,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::delete('/profile/photos/{photo}', [ProfilePhotoController::class, 'destroy'])
+    ->name('profile.photos.destroy');
+    
 require __DIR__.'/auth.php';
